5b884adf869a2a1f65b4ab514f0684ac510d1101
anonymousguy198 <anonguy912@gmail.com>
2020-09-05 15:54:38 +0530

    new file:   game 	modified:   how-to-use.txt 	modified:   map.c 	modified:   map.h 	modified:   platformer.c

--- map.c
+++ map.c
@@ -3,13 +3,16 @@
 
 
 
+
 //load the map
 struct MAP LoadMap(void){
-	
-	
 
-struct MAP map; 
+/*
+ *   indentation was really bad
+ *
+ */
 
+	struct MAP map; 
 	static char mapinfo[10][10]={
 	{'#','#','#','#','#','#','#','#','#','#',},
 	{'#','.','.','.','.','.','.','.','.','#',},
@@ -21,12 +24,13 @@ struct MAP map;
 	{'#','8','8','.','.','.','.','.','.','#',},
 	{'#','8','.','.','.','.','.','.','.','#',},
 	{'#','#','#','#','#','#','#','#','#','#',},
-};
-for(int x=0;x<+10;x++){
-		for(int y=0;y<=10;y++){
-		map.mapdata[y][x]=mapinfo[y][x];
-	}
+	};
 
+	for(int x=0;x<10;x++){
+		for(int y=0;y<10;y++){
+		map.mapdata[y][x]=mapinfo[y][x];
+		}
+	}     // missed a bracket here probably due to bad 
 	return map;
 }
 
@@ -35,8 +39,16 @@ for(int x=0;x<+10;x++){
 
 void RenderMap(struct MAP map,int cellx,int celly){
 
-	for(int x=0;x<+10;x++){
-		for(int y=0;y<=10;y++){
+	for(int x=0;x<10;x++){         
+/*
+ *  WHAT IS x<+10 supposed to be ??????????????????????????????
+ *
+ *  Am assuming you were going for x<=10 which is wrong as well
+ *  array starts from 0 and ends at TOTAL_SIZE-1
+ *  so condition should be x < 10
+ */
+
+		for(int y=0;y<10;y++){
 			if(map.mapdata[x][y]=='#'){DrawRectangle(x*cellx,y*celly,cellx,celly,RED);}
 			if(map.mapdata[x][y]=='8'){DrawRectangle(x*cellx,y*celly,cellx,celly,BLACK);}
 
--- map.h
+++ map.h
@@ -1,6 +1,14 @@
-extern void LoadMap(void);
-extern void RenderMap(struct MAP map,int cellx,int celly)
-
 struct MAP{
-		char mapdata[10][10];	
-	};
+    char mapdata[10][10];	
+};
+
+/*
+ *  struct needs to be declared before it is used
+ *
+ */
+
+struct MAP LoadMap(void);   //return type was void
+
+void RenderMap(struct MAP map,int cellx,int celly);  //<- missed a semicolon here. Reason for all that error
+
+// Dont make functions extern
